735489701d7d0e8399457fe935c4ac781e613798
Nam Nguyen <nam@namnm.com>
2019-01-25 21:58:07 +0700

    Fix jssip build error

--- web/webpack.js
+++ web/webpack.js
@@ -20,6 +20,7 @@ module.exports = {
           path.join(cwd, './app'),
           path.join(cwd, './index.web.js'),
           // Other node modules need to be transpiled:
+          path.join(cwd, './node_modules/jssip'),
           path.join(cwd, './node_modules/react-native-fcm'),
           path.join(cwd, './node_modules/react-native-progress-circle'),
           path.join(cwd, './node_modules/react-router-native'),
