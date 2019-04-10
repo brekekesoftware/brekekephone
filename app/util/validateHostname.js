480f7ee4e488db3dafe143e4e457e2ce81b4414f
Nam Nguyen <nam@namnm.com>
2019-04-10 20:34:26 +0700

    Pret

--- app/util/validateHostname.js
+++ app/util/validateHostname.js
@@ -1,9 +1,9 @@
 const validateHostname = hostname => {
   if (
-    /^\W/.test(hostname) // special character at the beginning => invalid
-    || /\W$/.test(hostname) // special character at the end => invalid
-    || /[^\w-]{2,}/.test(hostname) // two special characters near to each other => invalid
-    || hostname.length > 255 // too long => invalid
+    /^\W/.test(hostname) || // special character at the beginning => invalid
+    /\W$/.test(hostname) || // special character at the end => invalid
+    /[^\w-]{2,}/.test(hostname) || // two special characters near to each other => invalid
+    hostname.length > 255 // too long => invalid
   ) {
     return { status: false, message: 'Host name is invalid' };
   }
--- app/util/validatePort.js
+++ app/util/validatePort.js
@@ -1,9 +1,9 @@
 const validatePort = port => {
   if (
-      /\D/.test(port) // non numeric character => invalid
-      || /^0/.test(port) // starts with 0 => invalid
-      || parseInt(port) > 65535 // greater than max => invalid
-    ) {
+    /\D/.test(port) || // non numeric character => invalid
+    /^0/.test(port) || // starts with 0 => invalid
+    parseInt(port) > 65535 // greater than max => invalid
+  ) {
     return { status: false, message: 'Port is invalid' };
   }
   return { status: true, message: '' };
