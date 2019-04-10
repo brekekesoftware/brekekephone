f1c5bd7fc33aba10dcfec37fc102e7cbcf6fc177
Nam Nguyen <nam@namnm.com>
2019-04-10 21:58:43 +0700

    Improve hostname validation

--- app/util/validateHostname.js
+++ app/util/validateHostname.js
@@ -1,8 +1,9 @@
 const validateHostname = hostname => {
   if (
-    /^\W/.test(hostname) || // special character at the beginning => invalid
-    /\W$/.test(hostname) || // special character at the end => invalid
-    /[^\w-]{2,}/.test(hostname) || // two special characters near to each other => invalid
+    /[^\w-.]/.test(hostname) || // any special character exclude - and . => invalid
+    /^[-.]/.test(hostname) || // special character at the beginning => invalid
+    /[-.]$/.test(hostname) || // special character at the end => invalid
+    /\.{2,}/.test(hostname) || // two dots near to each other => invalid
     hostname.length > 255 // too long => invalid
   ) {
     return { status: false, message: 'Host name is invalid' };
