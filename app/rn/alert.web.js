eb2c728d9fd9b02b7998df241873eebe9a91b758
Nam Nguyen <nam@namnm.com>
2019-06-05 03:51:12 +0700

    Add more comment for the web alert

--- app/rn/alert.web.js
+++ app/rn/alert.web.js
@@ -2,18 +2,19 @@ import Swal from 'sweetalert2/dist/sweetalert2';
 import 'sweetalert2/dist/sweetalert2.css';
 
 export default {
-  alert: (title, text, [btn1, btn2], config) => {
+  // Only support [cancelBtn, okBtn]
+  alert: (title, text, [cancelBtn, okBtn], config) => {
     Swal.fire({
       title,
       text,
       showCancelButton: true,
-      confirmButtonText: btn2.text,
-      cancelButtonText: btn1.text,
+      confirmButtonText: okBtn.text,
+      cancelButtonText: cancelBtn.text,
     }).then(res => {
       if (res.value) {
-        btn2.onPress();
+        okBtn.onPress();
       } else {
-        btn1.onPress();
+        cancelBtn.onPress();
       }
     });
   },
