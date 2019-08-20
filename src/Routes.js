969c9f97ca5705133a4c429b4b3566a95e8bdc26
tuliba1996 <duancba@gmail.com>
2019-08-20 18:16:45 +0700

    move component not use

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import { StyleSheet, View } from 'react-native';
 import { Redirect, Route } from 'react-router';
 
 import NewCallPark from './components-Setting/NewCallPark';
-import TransferAttend from './components-Transfer/TransferAttend';
 import Auth from './components/auth';
 import WithoutStatusBar from './components/auth/WithoutStatusBar';
 import BuddyChatsNotify from './components/buddy-chats-notify';
@@ -45,7 +44,7 @@ const Routes = () => (
     <StatusBar />
     <WithoutStatusBar>
       <Route exact path="/" render={() => <Redirect to="/profiles/manage" />} />
-      <Route exact path="/profiles/manage" component={TransferAttend} />
+      <Route exact path="/profiles/manage" component={ProfilesManage} />
       <Route exact path="/profiles/create" component={ProfilesCreate} />
       <Route exact path="/profile/:profile/update" component={ProfileUpdate} />
       <Route exact path="/profile/:profile/signin" component={ProfileSignin} />
