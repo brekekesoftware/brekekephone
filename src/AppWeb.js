ad1c08a186cf804be1496a869888bf4f38714956
tuliba1996 <duancba@gmail.com>
2019-09-05 23:19:54 +0700

    page open app browser

--- src/AppWeb.js
+++ src/AppWeb.js
@@ -7,6 +7,11 @@ import { isAndroid, isIOS } from 'react-device-detect';
 
 import App from './App';
 import logoSrc from './assets/icon.png';
+import imgContent1 from './assets/image_1.png';
+import imgContent2 from './assets/image_2.png';
+import imgLogoAndroid from './assets/logo_android.png';
+import imgLogoApple from './assets/logo_apple.png';
+import imgLogoWeb from './assets/web_browser.png';
 
 @observer
 class AppWeb extends React.Component {
@@ -28,15 +33,37 @@ class AppWeb extends React.Component {
 
     return (
       <div className="AppWeb">
-        <h1>Brekeke Phone</h1>
-        <img src={logoSrc} alt="Brekeke Phone" />
+        <div class="AppWeb-Image">
+          <img src={imgContent1} alt="Brekeke Phone" />
+          <img src={imgContent2} alt="Brekeke Phone" />
+        </div>
+        <div class="AppWeb-Image ">
+          <img src={logoSrc} alt="Brekeke Phone" />
+        </div>
+        <div class="AppWeb-AppName">
+          <h2>Brekeke</h2>
+          <h5>PHONE</h5>
+        </div>
         <div />
         <a href={appUrl}>
-          <div className="AppWeb-Btn app">Open in app</div>
+          <div class="AppWeb-Btn app">
+            <img src={imgLogoApple} alt="Brekeke Phone" />
+            <h4>Open IOS App</h4>
+          </div>
+        </a>
+        <div />
+        <a href={appUrl}>
+          <div class="AppWeb-Btn app">
+            <img src={imgLogoAndroid} alt="Brekeke Phone" />
+            <h4>Open Android App</h4>
+          </div>
         </a>
         <div />
         <a href="./" onClick={this.enableWebVersion}>
-          <div className="AppWeb-Btn web">Open in browser</div>
+          <div class="AppWeb-Btn web">
+            <img src={imgLogoWeb} alt="Brekeke Phone" />
+            <h4>Open Web Directly</h4>
+          </div>
         </a>
       </div>
     );
