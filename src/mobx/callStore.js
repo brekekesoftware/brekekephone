fe671f780c3905f81273a2a03f290cdfc26e532e
tuliba1996 <duancba@gmail.com>
2019-09-06 12:44:47 +0700

    draw structure models call

--- src/mobx/callStore.js
+++ src/mobx/callStore.js
@@ -4,6 +4,29 @@ import BaseStore from './BaseStore';
 
 class CallStore extends BaseStore {
   @observable selectedId = '';
+
+  // id,
+  // status:
+  // 	'incoming'
+  //	'answered'
+  // partyNumber
+  // created
+  //@observable recentCalls = [];
+
+  // id
+  // status:
+  //	'incoming'
+  //	'answered'
+  //	'holding'
+  // partyName
+  // partyNumber
+  // recording
+  // localVideoEnabled
+  // loudspeaker
+  // pbxTenant
+  // pbxTalkerId
+  //
+  // @observable runningCalls = [];
 }
 
 export default new CallStore();
