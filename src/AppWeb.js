197542023ebef3fc63e5497fc05394969377aa6f
Nam Nguyen <nam@namnm.com>
2019-09-07 22:37:23 +0700

    Add new UI for selection page

--- src/AppWeb.js
+++ src/AppWeb.js
@@ -1,23 +1,28 @@
-import { action, observable } from 'mobx';
-import { observer } from 'mobx-react';
+import './AppWeb.scss';
+
+import { mdiAndroidHead, mdiApple, mdiWebBox } from '@mdi/js';
+import Icon from '@mdi/react';
 import React from 'react';
 import { isAndroid, isIOS } from 'react-device-detect';
 
 import App from './App';
-import './AppWeb.scss';
-import logoSrc from './Icon-180px.png';
+import brandSrc from './assets/brand.png';
+import iconSrc from './assets/icon.png';
+import mockupSrc from './assets/mockup.png';
 
-@observer
 class AppWeb extends React.Component {
-  @observable useWebVersion = !isIOS && !isAndroid;
-
-  @action enableWebVersion = e => {
+  state = {
+    useWebVersion: !isIOS && !isAndroid,
+  };
+  enableWebVersion = e => {
     e.preventDefault();
-    this.useWebVersion = true;
+    this.setState({
+      useWebVersion: true,
+    });
   };
 
   render() {
-    if (this.useWebVersion) {
+    if (this.state.useWebVersion) {
       return <App />;
     }
     const q = window.location.search;
@@ -26,16 +31,27 @@ class AppWeb extends React.Component {
       : `intent://open${q}#Intent;scheme=brekekeapp;package=com.brekeke.phone;end`;
     return (
       <div className="AppWeb">
-        <h1>Brekeke Phone</h1>
-        <img src={logoSrc} alt="Brekeke Phone" />
-        <div />
-        <a href={appUrl}>
-          <div className="AppWeb-Btn app">Open in app</div>
-        </a>
-        <div />
-        <a href="./" onClick={this.enableWebVersion}>
-          <div className="AppWeb-Btn web">Open in browser</div>
-        </a>
+        <img className="mockup" src={mockupSrc} alt="Brekeke Phone" />
+        <img className="icon" src={iconSrc} alt="Brekeke Phone" />
+        <div className="spacing" />
+        <img className="brand" src={brandSrc} alt="Brekeke Phone" />
+        <div className="btns">
+          <div className="btns-inner">
+            <a href={appUrl}>
+              <div className="btn app">
+                Open in app
+                <Icon path={isIOS ? mdiApple : mdiAndroidHead} />
+              </div>
+            </a>
+            <div className="spacing" />
+            <a href="." onClick={this.enableWebVersion}>
+              <div className="btn browser">
+                Open in browser
+                <Icon path={mdiWebBox} />
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
