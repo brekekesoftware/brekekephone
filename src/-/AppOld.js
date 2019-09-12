bad15f97c516e0377046bb40e00121094693ef20
Nam Nguyen <nam@namnm.com>
2019-09-12 15:21:53 +0700

    Move native-base to AppOld

--- src/-/AppOld.js
+++ src/-/AppOld.js
@@ -1,14 +1,22 @@
 import './oldNativeBaseTheme';
 
+import { StyleProvider } from 'native-base';
 import React from 'react';
+import * as Rn from 'react-native';
 
 import ApiProvider from './apis';
+import nativeBaseTheme from './nativeBaseTheme';
 import Routes from './Routes';
 
+// polyfill for native-base
+Rn.TouchableNativeFeedback.Ripple = color => color;
+
 const App = () => (
-  <ApiProvider>
-    <Routes />
-  </ApiProvider>
+  <StyleProvider style={nativeBaseTheme}>
+    <ApiProvider>
+      <Routes />
+    </ApiProvider>
+  </StyleProvider>
 );
 
 export default App;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './polyfill';
 
-import { StyleProvider } from 'native-base';
 import React from 'react';
 import { Platform } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { Route, Router, Switch } from 'react-router';
 
 import AppOld from './-/AppOld';
-import nativeBaseTheme from './-/nativeBaseTheme';
 import { history } from './-/routerStore';
 import PageProfileCreate from './-profile/PageProfileCreate';
 import PageProfileSignIn from './-profile/PageProfileSignIn';
@@ -31,15 +29,13 @@ class App extends React.Component {
   render() {
     return (
       <Router history={history}>
-        <StyleProvider style={nativeBaseTheme}>
-          <Switch>
-            <Route exact path="/" component={PageProfileSignIn} />
-            <Route path="/create-profile" component={PageProfileCreate} />
-            <Route path="/update-profile/:id" component={PageProfileUpdate} />
-            <Route path="/auth" component={AppOld} />
-            <Route component={Page404} />
-          </Switch>
-        </StyleProvider>
+        <Switch>
+          <Route exact path="/" component={PageProfileSignIn} />
+          <Route path="/create-profile" component={PageProfileCreate} />
+          <Route path="/update-profile/:id" component={PageProfileUpdate} />
+          <Route path="/auth" component={AppOld} />
+          <Route component={Page404} />
+        </Switch>
         <RootAlerts />
       </Router>
     );
