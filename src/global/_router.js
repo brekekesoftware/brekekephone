fb9a1aea0c892d709afaeb08a084811a4f11c3a8
Nam Nguyen <nam@namnm.com>
2019-09-12 16:50:50 +0700

    Shorten names

--- src/global/_router.js
+++ src/global/_router.js
@@ -5,31 +5,30 @@ import { Platform } from 'react-native';
 import g from './_';
 
 const r = new RouterStore();
-syncHistoryWithStore(
-  Platform.OS === 'web' ? createHashHistory() : createMemoryHistory(),
-  r,
-);
+const h = Platform.OS === 'web' ? createHashHistory() : createMemoryHistory();
+syncHistoryWithStore(h, r);
 
 // https://stackoverflow.com/a/24056766
 let goBackTimeoutId = 0;
 const goBack = fn => {
-  const h1 = r.location.pathname + r.location.search + r.location.hash;
-  r.history.goBack();
+  const l = r.location;
+  const h1 = l.pathname + l.search + l.hash;
+  h.goBack();
   if (goBackTimeoutId) {
     clearTimeout(goBackTimeoutId);
   }
   goBackTimeoutId = setTimeout(() => {
     goBackTimeoutId = 0;
-    const h2 = r.location.pathname + r.location.search + r.location.hash;
+    const h2 = l.pathname + l.search + l.hash;
     return h1 === h2 && fn();
   }, 100);
 };
 
 Object.assign(g, {
   router: r,
-  goToProfileSignIn: () => r.history.push(`/`),
-  goToProfileCreate: () => r.history.push(`/create-profile`),
-  goToProfileUpdate: id => r.history.push(`/update-profile/${id}`),
+  goToProfileSignIn: () => h.push(`/`),
+  goToProfileCreate: () => h.push(`/create-profile`),
+  goToProfileUpdate: id => h.push(`/update-profile/${id}`),
 });
 Object.entries(g).forEach(([k, v]) => {
   if (/^goTo/.test(k)) {
