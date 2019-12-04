ac2c504bf4f8f619a1520800503b4c4229a21a17
tuliba1996 <duancba@gmail.com>
2019-12-04 18:29:45 +0700

    refactor list phone books

--- src/-contact/PageContactPhonebook.js
+++ src/-contact/PageContactPhonebook.js
@@ -1,11 +1,14 @@
+import { mdiInformation, mdiPhone } from '@mdi/js';
 import debounce from 'lodash/debounce';
+import orderBy from 'lodash/orderBy';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import g from '../global';
+import Field from '../shared/Field';
 import Layout from '../shared/Layout';
-import ContactItem from './ContactItem';
+import UserItem from './UserItem';
 
 const numberOfContactsPerPage = 30;
 const formatPhoneNumber = number => number.replace(/\D+/g, ``);
@@ -30,6 +33,31 @@ class PageContactPhonebook extends React.Component {
 
   render() {
     const { contactIds } = this.state;
+    const phonebooks = contactIds.map(this.resolveContact);
+    console.warn(`phone`, phonebooks);
+    const map = {};
+
+    phonebooks.forEach(u => {
+      u.name = u.name || u.id;
+      let c0 = u.name.charAt(0).toUpperCase();
+      if (!/[A-Z]/.test(c0)) {
+        c0 = `#`;
+      }
+      if (!map[c0]) {
+        map[c0] = [];
+      }
+      map[c0].push(u);
+    });
+
+    let groups = Object.keys(map).map(k => ({
+      key: k,
+      phonebooks: map[k],
+    }));
+    groups = orderBy(groups, `key`);
+    groups.forEach(g => {
+      g.phonebooks = orderBy(g.phonebooks, `firstName`);
+    });
+
     return (
       <Layout
         footer={{
@@ -47,12 +75,42 @@ class PageContactPhonebook extends React.Component {
           },
         }}
       >
-        {contactIds.map(id => (
-          <ContactItem
-            key={id}
-            {...this.resolveContact(id)}
-            update={this.update}
-          />
+        {groups.map(_g => (
+          <React.Fragment key={_g.key}>
+            <Field isGroup label={_g.key} />
+            {_g.phonebooks.map((u, i) => (
+              <UserItem
+                function={[
+                  () =>
+                    g.openPicker({
+                      options: [
+                        {
+                          key: u.workNumber,
+                          label: u.workNumber || `Please add work number`,
+                          icon: mdiPhone,
+                        },
+                        {
+                          key: u.cellNumber,
+                          label: u.cellNumber || `Please add cell number`,
+                          icon: mdiPhone,
+                        },
+                        {
+                          key: u.homeNumber,
+                          label: u.homeNumber || `Please add home number`,
+                          icon: mdiPhone,
+                        },
+                      ],
+                      onSelect: this.call,
+                    }),
+                  () => this.update(u),
+                ]}
+                icon={[mdiPhone, mdiInformation]}
+                key={i}
+                last={i === _g.phonebooks.length - 1}
+                name={`${u.firstName} ${u.lastName}`}
+              />
+            ))}
+          </React.Fragment>
         ))}
       </Layout>
     );
@@ -172,6 +230,7 @@ class PageContactPhonebook extends React.Component {
     const { sip } = this.context;
     number = formatPhoneNumber(number);
     sip.createSession(number);
+    g.goToPageCallManage();
   };
 
   create = () => {
