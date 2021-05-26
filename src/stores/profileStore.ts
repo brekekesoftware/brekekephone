1e76e6d70624d10f245fb0fe8c3c2d63dc24d9c5
tatthangcntt <tatthangcntt@gmail.com>
2021-05-26 21:33:11 +0700

    Temporary hide push notification on web
    * quick fix: hide push notification with platform web
    
    * Update profileStore.ts

--- src/stores/profileStore.ts
+++ src/stores/profileStore.ts
@@ -2,6 +2,7 @@ import stringify from 'json-stable-stringify'
 import debounce from 'lodash/debounce'
 import uniqBy from 'lodash/uniqBy'
 import { action, computed, observable, runInAction } from 'mobx'
+import { Platform } from 'react-native'
 import { v4 as uuid } from 'react-native-uuid'
 
 import { SyncPnToken } from '../api/syncPnToken'
@@ -76,7 +77,7 @@ class ProfileStore {
     pbxPassword: '',
     pbxPhoneIndex: '',
     pbxTurnEnabled: false,
-    pushNotificationEnabled: true,
+    pushNotificationEnabled: Platform.OS === 'web' ? false : true,
     parks: ([] as any) as string[],
     ucEnabled: false,
     navIndex: -1,
