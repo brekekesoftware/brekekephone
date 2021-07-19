47a597b8a521f413ea46ea431f31278a854bbd33
Nam Nguyen <nam@namnm.com>
2021-07-19 23:13:51 +0700

    Release

--- android/app/src/main/java/com/brekeke/phone/L.java
+++ android/app/src/main/java/com/brekeke/phone/L.java
@@ -1,4 +1,4 @@
-package com.brekeke.phonedev;
+package com.brekeke.phone;
 
 public class L {
   public static String l = null;
--- src/api/pbx.ts
+++ src/api/pbx.ts
@@ -522,7 +522,7 @@ export class PBX extends EventEmitter {
     await this.client._pal('pnmanage', {
       command: 'set',
       service_id: '11',
-      application_id: 'com.brekeke.phonedev' + (voip ? '.voip' : ''),
+      application_id: 'com.brekeke.phone' + (voip ? '.voip' : ''),
       user_agent: 'react-native',
       username: username + (voip ? '@voip' : ''),
       device_id,
@@ -570,7 +570,7 @@ export class PBX extends EventEmitter {
     await this.client._pal('pnmanage', {
       command: 'remove',
       service_id: '11',
-      application_id: 'com.brekeke.phonedev' + (voip ? '.voip' : ''),
+      application_id: 'com.brekeke.phone' + (voip ? '.voip' : ''),
       user_agent: 'react-native',
       username: username + (voip ? '@voip' : ''),
       device_id,
