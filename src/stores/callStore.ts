861f3014ba02152400ea04593a553f767fca0515
tatthangcntt <tatthangcntt@gmail.com>
2021-08-06 12:00:18 +0700

    update check android

--- src/stores/callStore.ts
+++ src/stores/callStore.ts
@@ -123,7 +123,9 @@ export class CallStore {
     if (c && !c.callkeepAlreadyAnswered) {
       c.callkeepAlreadyAnswered = true
       c.answer()
-      IncomingCall.onConnectingCallSuccess(uuid)
+      if (Platform.OS === 'android') {
+        IncomingCall.onConnectingCallSuccess(uuid)
+      }
       this.recentPn = undefined
       console.error('SIP PN debug: answer by onCallKeepAnswerCall')
     } else if (this.recentPn?.uuid === uuid) {
