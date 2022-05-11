a035d5642c559d0c9610e88910c60caae5ce56f9
tatthangcntt <tatthangcntt@gmail.com>
2022-05-11 09:29:00 +0700

    read chunk blob

--- src/utils/saveBlob.ts
+++ src/utils/saveBlob.ts
@@ -19,33 +19,91 @@ export const saveBlob = (blob: Blob, name: string) => {
   }
   fr.readAsDataURL(blob)
 }
+// export const saveBlobFile = (id: string, topic_id: string, type?: string) => {
+//   const fileType = type || 'image'
+//   return new Promise(async (resolve, reject) => {
+//     const fr = new FileReader()
+//     try {
+//       const data: Blob = (await uc.acceptFile(id)) as Blob
+//       fr.onloadend = async () => {
+//         const r = fr.result as string
+//         const b64 = r.replace(/^data:.*base64,/, '')
+//         const p = `${RNFS.DocumentDirectoryPath}/${id}.${
+//           fileType === 'image' ? 'jpeg' : 'mp4'
+//         }`
+//         try {
+//           await RNFS.writeFile(p, b64, 'base64')
+//           resolve(p)
+//         } catch (err) {
+//           console.error(`saveBlobFile RNFS.writeFile err: ${err}`)
+//           reject(err)
+//         }
+//       }
+//       fr.onerror = err => {
+//         console.error(`saveBlobFile onerror err: ${err}`)
+//         reject(err)
+//       }
+//       fr.readAsDataURL(data)
+//     } catch (err) {
+//       console.error(`saveBlobFile catch err: ${err}`)
+//       reject(err)
+//     }
+//   })
+// }
+
+const readChunkFile = (p: string, pos: number, data: Blob) => {
+  console.error('readChunkFile::', data.size)
+  return new Promise(async (a, b) => {
+    const fr = new FileReader()
+    console.error('readChunkFile::', data)
+    fr.readAsDataURL(data)
+    fr.onloadend = async () => {
+      const r = fr.result as string
+      const b64 = r.replace(/^data:.*base64,/, '')
+      console.error('dataFile::', b64)
+      try {
+        if (pos === 0) {
+          await RNFS.writeFile(p, b64, 'base64')
+        } else {
+          await RNFS.appendFile(p, b64, 'base64')
+        }
+        a(p)
+      } catch (err) {
+        console.error(`saveBlobFile RNFS.writeFile err: ${err}`)
+        b(err)
+      }
+    }
+  })
+}
 export const saveBlobFile = (id: string, topic_id: string, type?: string) => {
   const fileType = type || 'image'
   return new Promise(async (resolve, reject) => {
-    const fr = new FileReader()
+    // const fr = new FileReader()
+    // Best to add 2 so it strips == from all chunks
+    // except from the last chunk
     try {
       const data: Blob = (await uc.acceptFile(id)) as Blob
-      fr.onloadend = async () => {
-        const r = fr.result as string
-        const b64 = r.replace(/^data:.*base64,/, '')
-        const p = `${RNFS.DocumentDirectoryPath}/${id}.${
-          fileType === 'image' ? 'jpeg' : 'mp4'
-        }`
-        try {
-          await RNFS.writeFile(p, b64, 'base64')
-          resolve(p)
-        } catch (err) {
-          console.error(`saveBlobFile RNFS.writeFile err: ${err}`)
-          reject(err)
-        }
+      const chunkSize = 1024 * 2 // 4227529
+      // var pos = 0
+      const p = `${RNFS.DocumentDirectoryPath}/${id}.${
+        fileType === 'image' ? 'jpeg' : 'mp4'
+      }`
+      console.error('Blob::Size::', data.size)
+
+      for (let pos = 0; pos < data.size; pos += chunkSize) {
+        const blob: Blob = data.slice(pos, pos + chunkSize)
+        console.error(
+          'Blob::Pos::',
+          pos,
+          'Blob::Size::',
+          data.size,
+          'Blob::Data::Size::',
+          blob,
+        )
+        await readChunkFile(p, pos, blob)
       }
-      fr.onerror = err => {
-        console.error(`saveBlobFile onerror err: ${err}`)
-        reject(err)
-      }
-      fr.readAsDataURL(data)
+      resolve(p)
     } catch (err) {
-      console.error(`saveBlobFile catch err: ${err}`)
       reject(err)
     }
   })
