13e546d30979ce4f9a03399c3ad14d1175fc2db7
tatthangcntt <tatthangcntt@gmail.com>
2022-06-10 18:43:29 +0700

    Fix issue no audio ios early media

--- src/components/CallVoicesUI.tsx
+++ src/components/CallVoicesUI.tsx
@@ -1,10 +1,20 @@
 import { Component } from 'react'
-import { Platform } from 'react-native'
+import { Platform, StyleSheet } from 'react-native'
 import IncallManager from 'react-native-incall-manager'
+import Video from 'react-native-video'
 
 import { callStore } from '../stores/callStore'
 import { BrekekeUtils } from '../utils/RnNativeModules'
 
+const css = StyleSheet.create({
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+})
 export class IncomingItem extends Component {
   ringtonePlaying = false
   async componentDidMount() {
@@ -30,18 +40,15 @@ export class IncomingItem extends Component {
 }
 
 export class OutgoingItem extends Component {
-  componentDidMount() {
-    IncallManager.startRingback('_BUNDLE_')
-  }
-  componentWillUnmount() {
-    IncallManager.stopRingback()
-    if (Platform.OS === 'android') {
-      // Bug speaker auto turn on after call stopRingtone/stopRingback
-      IncallManager.setForceSpeakerphoneOn(callStore.isLoudSpeakerEnabled)
-    }
-  }
   render() {
-    return null
+    return (
+      <Video
+        source={require('../assets/incallmanager_ringback.mp3')}
+        style={css.video}
+        repeat={true}
+        playInBackground={true}
+      />
+    )
   }
 }
 export class OutgoingItemWithSDP extends Component<{
