3a3e0acbef09cd096513568eb1219fa4c6dd4305
Nam Nguyen <nam@namnm.com>
2022-11-23 14:18:33 +0700

    nam

--- src/utils/checkImageURL.ts
+++ src/utils/checkImageURL.ts
@@ -1,11 +1,13 @@
 import Url from 'url-parse'
 
 export const checkImageUrl = (url: string) => {
-  // exception get image from UC: https://apps.brekeke.com:8443/uc/image?ACTION=DOWNLOAD&tenant=nam&user=1003&dlk=ltt3&SIZE=40
-  const urlParams = new Url(url.toLowerCase())
-  console.error({ pathname: urlParams.pathname })
+  url = url.toLowerCase()
+  const u = new Url(url)
+  console.log(`checkImageUrl pathname=${u.pathname}`)
   return (
-    /\.(jpeg|jpg|gif|png)$/.test(urlParams.pathname) ||
-    url.toLowerCase().includes('/uc/image?ACTION=DOWNLOAD&tenant'.toLowerCase())
+    /\.(jpeg|jpg|gif|png)$/.test(u.pathname) ||
+    // default image url from uc
+    // https://apps.brekeke.com:8443/uc/image?ACTION=DOWNLOAD&tenant=nam&user=1003&dlk=ltt3&SIZE=40
+    url.includes('/uc/image?action=download&tenant')
   )
 }
