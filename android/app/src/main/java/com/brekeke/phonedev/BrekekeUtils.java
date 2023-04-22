e7a498ce448c2a5610e98cece07dceb54aed1c2b
Nam Nguyen <nam@namnm.com>
2023-04-23 02:45:48 +0700

    nam

--- android/app/src/main/java/com/brekeke/phonedev/BrekekeUtils.java
+++ android/app/src/main/java/com/brekeke/phonedev/BrekekeUtils.java
@@ -213,10 +213,10 @@ public class BrekekeUtils extends ReactContextBaseJavaModule {
               return;
             }
             userActions.put(uuid, "display");
-            activitiesSize++;
-            if (activitiesSize == 1) {
+            if (activitiesSize == 0) {
               firstShowCallAppActive = isAppActive || isAppActiveLocked;
             }
+            activitiesSize++;
             Intent i = new Intent(c, IncomingCallActivity.class);
             i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
             i.putExtra("uuid", uuid);
