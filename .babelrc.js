a0b5be1fe7a1b51fb6eeb4a1a4e72553b3160c68
tatthangcntt <tatthangcntt@gmail.com>
2023-06-30 10:15:50 +0700

    Fix issue: Typeerror: undefined is not an object (evaluating 'props.getitem')

--- .babelrc.js
+++ .babelrc.js
@@ -8,6 +8,8 @@ module.exports = {
     ],
   ],
   plugins: [
+    // Ref: https://github.com/facebook/react-native/issues/36828#issuecomment-1589462227
+    '@babel/plugin-transform-flow-strip-types',
     intlBabelPlugin,
     '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
