bb73c65a0eedbee60e91ecf13676bd398baa87d2
Nam Nguyen <nam@namnm.com>
2023-11-03 02:06:01 +0700

    Release
    (cherry picked from commit 117892152fe55a2034815ca7486fcec8ca955213)
    (cherry picked from commit 33db0b3b4ce706e18ecc8ed94e0e215d6419897f)
    (cherry picked from commit 6c16b466f0e29cad34f02e1bdf06ed18a235e873)

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import type { WebViewSource } from 'react-native-webview/lib/WebViewTypes'
 
 export const successConnectCheckPeriod = 600000 // 10 minutes
 export const fcmApplicationId = '22177122297'
-export const bundleIdentifier = 'com.brekeke.phonedev'
+export const bundleIdentifier = 'com.brekeke.phone'
 
 export const buildWebViewSource = (uri: string): WebViewSource => ({
   uri,
