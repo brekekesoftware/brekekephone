b5d38acbe863d25927604bdea20ddf9002299e0a
tatthangcntt <tatthangcntt@gmail.com>
2024-02-23 15:01:43 +0700

    - Handle ask user set default phone app - Handle make call when user make call from other app

---
+++ android/app/src/main/java/com/brekeke/phonedev/CallService.java
@@ -0,0 +1,49 @@
+package com.brekeke.phonedev;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.telecom.Call;
+import android.telecom.InCallService;
+import android.telecom.TelecomManager;
+import android.util.Log;
+
+public class CallService extends InCallService {
+
+ @Override
+ public void onCallAdded(Call call) {
+  super.onCallAdded(call);
+  call.registerCallback(callCallback);
+  // We can start our call Activity here
+  try {
+   if (call.getDetails().getHandlePresentation() == TelecomManager.PRESENTATION_ALLOWED) {
+    // This is an outgoing call
+    String number = call.getDetails().getHandle().getSchemeSpecificPart();
+    if (number == null) {
+     return;
+    }
+    Uri phoneNumberUri = Uri.parse("tel:" + number);
+    Intent intent = new Intent(Intent.ACTION_VIEW, phoneNumberUri);
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+    startActivity(intent);
+   } else {
+    // Todo with incoming call
+   }
+  } catch (Exception e) {
+
+  }
+
+ }
+
+ @Override
+ public void onCallRemoved(Call call) {
+  super.onCallRemoved(call);
+  call.unregisterCallback(callCallback);
+ }
+
+ private Call.Callback callCallback = new Call.Callback() {
+  @Override
+  public void onStateChanged(Call call, int state) {
+  }
+ };
+}
