62d43f19b06816991014acd25b515d6103db97dc
Duy Phann <phanthanhduy.jin@gmail.com>
2024-03-26 10:32:40 +0700

    update: identifier

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { WebViewSource } from 'react-native-webview/lib/WebViewTypes'
 
 export const fcmApplicationId = '22177122297'
-export const bundleIdentifier = 'com.brekeke.phonedev'
+export const bundleIdentifier = 'com.brekeke.exphonedev'
 
 export const buildWebViewSource = (uri: string): WebViewSource => ({
   uri,
