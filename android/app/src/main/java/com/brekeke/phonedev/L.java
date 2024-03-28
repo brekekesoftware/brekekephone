728df4959306afad6db69377e492d409f3685372
tatthangcntt <tatthangcntt@gmail.com>
2024-03-28 22:35:29 +0700

    Fix issue: The app get hang when user cancel reuqets set default phone app

--- android/app/src/main/java/com/brekeke/phonedev/L.java
+++ android/app/src/main/java/com/brekeke/phonedev/L.java
@@ -81,13 +81,6 @@ public class L {
     return "ja".equals(l) ? "必要な権限" : "Set Default Phone App";
   }
 
-  public static String messagePermissionSetDefaultPhoneApp() {
-    return "ja".equals(l)
-        ? "Brekeke Phoneをデフォルトの電話アプリとして設定するには、設定＞brekeke phone＞電話アプリに移動して設定してください。"
-        : "To set the Brekeke Phone app as the default phone app, please go to settings > brekeke"
-            + " phone > Phone app to configure.";
-  }
-
   public static String close() {
     return "ja".equals(l) ? "閉じる" : "Close";
   }
