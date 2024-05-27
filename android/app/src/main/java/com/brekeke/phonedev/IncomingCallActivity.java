56d2a5a700638b3f9de563e997d1455a487e5a34
tatthangcntt <tatthangcntt@gmail.com>
2024-05-27 09:58:32 +0700

    Fix android it should display PhoneAppli image correctly during 3PCC calls with auto-answer

--- android/app/src/main/java/com/brekeke/phonedev/IncomingCallActivity.java
+++ android/app/src/main/java/com/brekeke/phonedev/IncomingCallActivity.java
@@ -933,10 +933,9 @@ public class IncomingCallActivity extends Activity implements View.OnClickListen
   public void onCallConnected() {
     if (!answered) {
       setCallAnswered();
-    }
-    if (vCallManageLoading.getVisibility() == View.GONE) {
       return;
     }
+
     long answeredAt = System.currentTimeMillis();
     startTimer(answeredAt);
     vCallManageLoading.setVisibility(View.GONE);
@@ -1080,6 +1079,9 @@ public class IncomingCallActivity extends Activity implements View.OnClickListen
   public TimerTask timerTask;
 
   public void startTimer(long answeredAt) {
+    if (timerTask != null) {
+      return;
+    }
     timerTask =
         new TimerTask() {
           @Override
