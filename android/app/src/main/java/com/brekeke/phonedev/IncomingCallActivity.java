3f305d89cfcf48f956ca601b91ccdc5b205fa1bd
tatthangcntt <tatthangcntt@gmail.com>
2024-05-27 10:03:30 +0700

    Fix android it should display well ringing image with PhoneAppli

--- android/app/src/main/java/com/brekeke/phonedev/IncomingCallActivity.java
+++ android/app/src/main/java/com/brekeke/phonedev/IncomingCallActivity.java
@@ -625,7 +625,6 @@ public class IncomingCallActivity extends Activity implements View.OnClickListen
   }
 
   public void handleShowAvatarTalking() {
-    destroyAvatarWebView();
     if (BrekekeUtils.isImageUrl(talkingAvatar)) {
       webViewAvatarTalking.setVisibility(View.GONE);
       imgAvatarTalking.setVisibility(View.VISIBLE);
@@ -928,6 +927,7 @@ public class IncomingCallActivity extends Activity implements View.OnClickListen
     vHeaderIncomingCall.setVisibility(View.GONE);
     vCallManage.setVisibility(View.VISIBLE);
     vNavHeader.setVisibility(View.VISIBLE);
+    destroyAvatarWebView();
   }
 
   public void onCallConnected() {
