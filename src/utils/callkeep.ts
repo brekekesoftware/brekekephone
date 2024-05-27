73eaefd174e8225c86648a56460b8b76678fa5de
Nam Nguyen <nam@namnm.com>
2024-05-27 12:27:38 +0700

    Hotfix android set talking avatar on auto answer

--- src/utils/callkeep.ts
+++ src/utils/callkeep.ts
@@ -243,17 +243,20 @@ export const setupCallKeepEvents = async () => {
 
   // events from our custom BrekekeUtils module
   const eventEmitter = new NativeEventEmitter(BrekekeUtils)
-  eventEmitter.addListener('answerCall', (uuid: string) => {
+  eventEmitter.addListener('answerCall', async (uuid: string) => {
     // should update the native android UI here to fix a case with auto answer
     const c = cs.calls.find(_ => _.callkeepUuid === uuid && _.answered)
     if (c) {
+      if (Platform.OS === 'android') {
+        // with auto answer, talkingAvatar takes too long to update
+        BrekekeUtils.setTalkingAvatar(
+          uuid,
+          c.talkingImageUrl,
+          c.partyImageSize === 'large',
+        )
+        await waitTimeout(17)
+      }
       BrekekeUtils.onCallConnected(uuid)
-      // with auto answer, talkingAvatar takes too long to update
-      BrekekeUtils.setTalkingAvatar(
-        uuid,
-        c.talkingImageUrl,
-        c.partyImageSize === 'large',
-      )
     }
     cs.onCallKeepAnswerCall(uuid.toUpperCase())
     RNCallKeep.setOnHold(uuid, false)
