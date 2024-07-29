82386f5734e1e7c93ee883bf0a5a6cc19e776dc4
tatthangcntt <tatthangcntt@gmail.com>
2024-07-29 17:13:36 +0700

    Fix issue: Webphone can not load video in Nightly browser ver 130.0a1

--- src/utils/getAudioVideoPermission.ts
+++ src/utils/getAudioVideoPermission.ts
@@ -8,12 +8,20 @@ export const getAudioVideoPermission = () => {
   const eb = (err: MediaStreamError) => {
     /* TODO */
   }
-  // @ts-ignore
-  const p = window.navigator.getUserMedia(
-    { audio: true, video: true },
-    cb,
-    eb,
-  ) as any as Promise<MediaStream>
+  let p: Promise<MediaStream>
+  if (navigator.mediaDevices?.getUserMedia) {
+    // New syntax
+    p = navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+  } else {
+    // Old syntax
+    // @ts-ignore
+    p = window.navigator.getUserMedia(
+      { audio: true, video: true },
+      cb,
+      eb,
+    ) as any as Promise<MediaStream>
+  }
+
   if (p?.then) {
     p.then(cb).catch(eb)
   }
