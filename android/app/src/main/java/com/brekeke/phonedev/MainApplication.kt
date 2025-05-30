70121d4950540309daf740d8d8dfca116c2ff763
tatthangcntt <tatthangcntt@gmail.com>
2025-05-30 22:06:29 +0700

    Update fix build failure

---
+++ android/app/src/main/java/com/brekeke/phonedev/MainApplication.kt
@@ -0,0 +1,61 @@
+package com.brekeke.phonedev
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
+import com.facebook.soloader.SoLoader
+
+class MainApplication : Application(), ReactApplication {
+
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    add(BrekekeUtilsReactPackage())
+                }
+
+            override fun getJSMainModuleName(): String = "index"
+
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+            override val isNewArchEnabled: Boolean = false;
+            override val isHermesEnabled: Boolean = false;
+        }
+
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+    override fun onCreate() {
+        super.onCreate()
+        // Ép disable bridgeless để tránh lỗi libreact_featureflagsjni.so
+        System.setProperty("react.bridgeless", "false")
+        SoLoader.init(this, OpenSourceMergedSoMapping)
+
+    }
+
+    @SuppressLint("WrongConstant")
+    override fun registerReceiver(
+        receiver: BroadcastReceiver?,
+        filter: IntentFilter
+    ): Intent? {
+        return if (Build.VERSION.SDK_INT >= 34 && applicationInfo.targetSdkVersion >= 34) {
+            super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED)
+        } else {
+            super.registerReceiver(receiver, filter)
+        }
+    }
+}
