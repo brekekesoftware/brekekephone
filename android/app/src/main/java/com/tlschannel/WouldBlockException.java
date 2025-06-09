2f38e4f2fbf5810e00e33a5d9a2a93f9e24abde7
Hoang Vu <hoang.vu@namlongsoft.net>
2025-06-09 16:33:58 +0700

    Remove tlschanel folder on Android native

--- android/app/src/main/java/com/tlschannel/WouldBlockException.java
+++
@@ -1,9 +0,0 @@
-package com.tlschannel;
-
-/**
- * Signals that some IO operation cannot continue because the channel is in non blocking mode and
- * some blocking would otherwise happen.
- */
-public class WouldBlockException extends TlsChannelFlowControlException {
-  private static final long serialVersionUID = -1881728208118024998L;
-}
--- android/app/src/main/java/com/tlschannel/async/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package enables the usage of TLS Channel as an {@link
- * java.nio.channels.AsynchronousByteChannel}.
- */
-package com.tlschannel.async;
