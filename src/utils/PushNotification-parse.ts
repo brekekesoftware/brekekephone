3de2d2141973759e1812635276402ffc1558ef9f
tatthangcntt <tatthangcntt@gmail.com>
2025-10-06 17:04:29 +0700

    should update the UI for auto answer when PN arrives after IncomingCallActivity is created.

--- src/utils/PushNotification-parse.ts
+++ src/utils/PushNotification-parse.ts
@@ -324,6 +324,10 @@ export const parse = async (
     console.log(`SIP PN debug: getPendingUserAction=${action}`)
     if (action === 'answerCall') {
       ctx.call.onCallKeepAnswerCall(n.callkeepUuid)
+      // Update UI immediately after the call has been auto-answered but PN data arrives later
+      if (n.sipPn.autoAnswer) {
+        BrekekeUtils.onCallConnected(n.callkeepUuid)
+      }
     } else if (action === 'rejectCall') {
       ctx.call.onCallKeepEndCall(n.callkeepUuid)
     }
