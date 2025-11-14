d3a23bc26458556d314a7b814e576dbb18a6d370
Nam Nguyen <nam@namnm.com>
2025-11-14 10:18:20 +0700

    Release
    (cherry picked from commit 530ccb85f1a9e7f0559b5d94d8d956f8570a7b24)

---
+++ android/app/src/main/java/com/brekeke/phone/MainActivity.kt
@@ -0,0 +1,144 @@
+package com.brekeke.phone
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.KeyEvent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.NonNull
+import com.brekeke.phone.utils.Ctx
+import com.brekeke.phone.utils.Emitter
+import com.brekeke.phone.utils.Perm
+import com.brekeke.phone.utils.Ringtone
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import io.wazo.callkeep.RNCallKeepModule
+
+class MainActivity : ReactActivity() {
+  // ==========================================================================
+  // set/unset BrekekeUtils.main
+  override fun onStart() {
+    BrekekeUtils.main = this
+    super.onStart()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    // permissions
+    Perm.resolve()
+    // call history
+    // TODO: temporary disabled
+    if (true) return
+    val b = intent.extras ?: return
+    val phone = b.getString("extra_phone")
+    if (phone.isNullOrEmpty()) return
+    intent.removeExtra("extra_phone")
+    handleMakeCall(phone)
+  }
+
+  override fun onDestroy() {
+    BrekekeUtils.main = null
+    BrekekeUtils.dialerCheckState = DialerCheckState.IDLE
+    Ringtone.stop()
+    Emitter.emit("onDestroyMainActivity", "")
+    super.onDestroy()
+  }
+
+  // ==========================================================================
+  // check if notification pressed
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    if (BrekekeUtils.main == null) {
+      BrekekeUtils.main = this
+    }
+    // handle default dialer
+    BrekekeUtils.defaultDialerLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+          checkSetDefaultDialerResult(result.resultCode)
+        }
+
+    if (isOpenedFromLauncherIcon(intent)) {
+      BrekekeUtils.checkDefaultDialer()
+    }
+    // handle call from other app
+    handleIntent(intent)
+  }
+
+  private fun isOpenedFromLauncherIcon(intent: Intent?): Boolean {
+    return intent?.action == Intent.ACTION_MAIN &&
+        intent.categories?.contains(Intent.CATEGORY_LAUNCHER) == true
+  }
+
+  // ==========================================================================
+  // stop ringtone on any press and custom back btn handler
+  override fun dispatchKeyEvent(e: KeyEvent): Boolean {
+    val k = e.keyCode
+    val a = e.action
+    Emitter.debug("MainActivity.onKeyDown k=$k a=$a")
+    Ringtone.stop()
+    if (k == KeyEvent.KEYCODE_BACK || k == KeyEvent.KEYCODE_SOFT_LEFT) {
+      if (a == KeyEvent.ACTION_DOWN) {
+        Emitter.emit("onBackPressed", "")
+      }
+      return true
+    }
+    return super.dispatchKeyEvent(e)
+  }
+
+  // ==========================================================================
+  // react-native config
+  override fun getMainComponentName(): String = "BrekekePhone"
+
+  override fun createReactActivityDelegate(): ReactActivityDelegate {
+    return object : ReactActivityDelegate(this, mainComponentName) {
+      override fun isFabricEnabled(): Boolean = false
+    }
+  }
+
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    setIntent(intent)
+    // handle call from other app
+    handleIntent(intent)
+  }
+
+  override fun onRequestPermissionsResult(
+      requestCode: Int,
+      @NonNull permissions: Array<String>,
+      @NonNull grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    when (requestCode) {
+      RNCallKeepModule.REQUEST_READ_PHONE_STATE ->
+          RNCallKeepModule.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+  }
+
+  // ==========================================================================
+  // handle call from other app
+  private fun handleIntent(intent: Intent) {
+    if (Intent.ACTION_VIEW == intent.action || intent.hasCategory(Intent.CATEGORY_BROWSABLE)) {
+      val data: Uri? = intent.data
+      if (data == null || data.scheme != "tel") return
+      val phoneNumber = data.schemeSpecificPart
+      if (phoneNumber.isNullOrEmpty()) return
+      handleMakeCall(phoneNumber)
+    }
+  }
+
+  private fun handleMakeCall(phone: String) {
+    if (Emitter.emit("makeCall", phone)) {
+      return
+    }
+    var h = Ctx.h()
+    h.postDelayed({ Emitter.emit("makeCall", phone) }, 5000)
+  }
+
+  private fun checkSetDefaultDialerResult(resultCode: Int) {
+    when (resultCode) {
+      RESULT_CANCELED ->
+          BrekekeUtils.resultDefaultDialer("Permission to set default phone app was canceled")
+      RESULT_OK -> BrekekeUtils.resultDefaultDialer("Default dialer set successfully")
+    }
+  }
+}
---
+++ android/app/src/main/java/com/brekeke/phone/utils/MonitorConnection.java
@@ -0,0 +1,70 @@
+package com.brekeke.phone.utils;
+
+import android.os.Handler;
+import com.brekeke.phone.lpc.BrekekeLpcService;
+
+public class MonitorConnection {
+  private static final long DEFAULT_TIMEOUT_MS = 20000;
+  private Handler handler;
+  private final long timeoutMs;
+  private int countReconnect = 0;
+  private boolean isConnected = false;
+  private final Runnable timeoutTask = this::onDisconnected;
+
+  public MonitorConnection() {
+    this(DEFAULT_TIMEOUT_MS);
+  }
+
+  public MonitorConnection(long timeoutMs) {
+    this.timeoutMs = timeoutMs;
+    handler = Ctx.h();
+  }
+
+  public void onConnected() {
+    onMessageReceived();
+  }
+
+  public void onMessageReceived() {
+    countReconnect = 0;
+    updateState(true);
+    resetTimer();
+  }
+
+  public void onDisconnected() {
+    updateState(false);
+    resetTimer();
+  }
+
+  private void resetTimer() {
+    cancelTimer();
+    handler.postDelayed(timeoutTask, timeoutMs);
+  }
+
+  public void cancelTimer() {
+    handler.removeCallbacks(timeoutTask);
+  }
+
+  private void updateState(boolean connected) {
+    if (isConnected != connected) {
+      isConnected = connected;
+      Emitter.debug("LPC connection: " + (connected ? "CONNECTED" : "DISCONNECTED"));
+    }
+
+    if (!connected) {
+      var iService = BrekekeLpcService.iService;
+      if (countReconnect > 1 && !BrekekeLpcService.isReconnectByNetworkChange && iService != null) {
+        countReconnect = 0;
+        var ctx = Ctx.app();
+        if (ctx != null) {
+          BrekekeLpcService.isServiceStarted = false;
+          ctx.startForegroundService(iService);
+        }
+      }
+      countReconnect++;
+    }
+  }
+
+  public boolean isConnected() {
+    return isConnected;
+  }
+}
