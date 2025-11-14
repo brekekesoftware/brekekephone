5e0b516d014035ed686dd7237815f174b007eee0
Nam Nguyen <nam@namnm.com>
2025-11-14 10:18:20 +0700

    Release

---
+++ android/app/src/main/java/com/brekeke/phone/utils/NetworkUtils.java
@@ -0,0 +1,33 @@
+package com.brekeke.phone.utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import androidx.core.content.ContextCompat;
+import java.util.ArrayList;
+
+public class NetworkUtils {
+  public static boolean matchSsid(Context ctx, ArrayList<String> s) {
+    WifiManager wifiManager = (WifiManager) ctx.getSystemService(Context.WIFI_SERVICE);
+    WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+    var w = wifiInfo.getSSID().replace("\"", "");
+    return s.contains(w);
+  }
+
+  public static boolean checkAllowAllTheTime(Context ctx) {
+    boolean hasForeground =
+        ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED;
+
+    boolean hasBackground = true;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      hasBackground =
+          ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+              == PackageManager.PERMISSION_GRANTED;
+    }
+    return hasForeground && hasBackground;
+  }
+}
