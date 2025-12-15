f7e75fc08b9722cf1de63724fc4ee414302223c4
Nam Nguyen <nam@namnm.com>
2025-12-15 11:44:29 +0700

    Rename static ringtone display

--- src/utils/BrekekeUtils.ts
+++ src/utils/BrekekeUtils.ts
@@ -4,6 +4,7 @@ import { NativeModules } from 'react-native'
 
 import { isWeb } from '#/config'
 import type { TCallKeepAction } from '#/stores/callStore'
+import { intl } from '#/stores/intl'
 
 type TBrekekeUtils = {
   // ==========================================================================
@@ -216,6 +217,12 @@ export const staticRingtones = [
   'incallmanager_ringtone',
   // strong typing to make sure not missing static ringtone mp3
 ] as const
+
+export const staticRingtoneMap: {
+  [k in (typeof staticRingtones)[number]]: () => string
+} = {
+  incallmanager_ringtone: () => intl`Brekeke ringtone`,
+}
 
 export type RemoteStream = {
   vId: string
