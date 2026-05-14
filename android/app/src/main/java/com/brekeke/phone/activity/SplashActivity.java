dd2983321f76719f6e7ba8fe854e0eb68dce2113
Nam Nguyen <nam@namnm.com>
2026-05-14 22:49:30 +0700

    Release

---
+++ android/app/src/main/java/com/brekeke/phone/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.brekeke.phone.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.brekeke.phone.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Intent intent = new Intent(this, MainActivity.class);
+    // https://github.com/crazycodeboy/react-native-splash-screen/issues/289
+    Bundle extras = getIntent().getExtras();
+    if (extras != null) {
+      intent.putExtras(extras);
+    }
+    startActivity(intent);
+    finish();
+  }
+}
---
+++ android/app/src/main/java/com/brekeke/phone/lpc/LpcModel.java
@@ -0,0 +1,55 @@
+package com.brekeke.phone.lpc;
+
+import java.util.ArrayList;
+
+public class LpcModel {
+  public class User {
+    String uuid;
+    String uuid2;
+    String deviceName;
+    final String appid = "22177122297";
+
+    User(String token, String token2, String userName) {
+      this.uuid = token + "$pn-gw@" + userName;
+      this.uuid2 = token2 + "$pn-gw@" + userName + "@voip";
+      this.deviceName = userName;
+    }
+  }
+
+  public class Settings {
+    String host;
+    int port;
+    String tlsKeyHash = "";
+    String localSsids;
+    ArrayList<String> remoteSsids;
+    String mobileCountryCode = "";
+    String mobileNetworkCode = "";
+    String trackingAreaCode = "";
+    boolean enabled = true;
+    String token;
+    String userName;
+
+    public Settings(String host, int port, String tlsKeyHash, String token, String userName) {
+      this.host = host;
+      this.port = port;
+      this.tlsKeyHash = tlsKeyHash;
+      this.token = token;
+      this.userName = userName;
+    }
+
+    public Settings(
+        String host,
+        int port,
+        String tlsKeyHash,
+        String token,
+        String userName,
+        ArrayList<String> remoteSsids) {
+      this.host = host;
+      this.port = port;
+      this.tlsKeyHash = tlsKeyHash;
+      this.token = token;
+      this.userName = userName;
+      this.remoteSsids = remoteSsids;
+    }
+  }
+}
