c8e01eae343a8f392e1d68b9d6bd3476998726cf
Nam Nguyen <nam@namnm.com>
2026-05-14 22:49:30 +0700

    Release

---
+++ android/app/src/main/java/com/brekeke/phone/BrekekeUtilsReactPackage.java
@@ -0,0 +1,23 @@
+package com.brekeke.phone;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class BrekekeUtilsReactPackage implements ReactPackage {
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext ctx) {
+    var l = new ArrayList<NativeModule>();
+    l.add(new BrekekeUtils(ctx));
+    return l;
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext ctx) {
+    return Collections.emptyList();
+  }
+}
